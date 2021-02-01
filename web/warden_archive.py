570475278bd0a8e29f2a6114517ac7a2d35af400
Václav Bartoš <bartos@cesnet.cz>
2021-02-01 14:47:41 +0100

    web: allow to specify the number of days to show
    and moved the plot position

--- web/warden_archive.py
+++ web/warden_archive.py
@@ -6,19 +6,22 @@ STATS_DIR = PREFIX + '/stats'
 DAYS = 14
 
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 from datetime import date, timedelta
 
 application = app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # optional param: days=<int>
 def main():
     data = [] # array of (date, cnt, cnt_test)
-    
+
+    n_days = request.args.get("days", default=DAYS, type=int)
+    n_days = max(min(n_days, 1000), 1) # limit to interval [1,1000]
+
     # Load data from last DAYS days
     today = date.today()
-    for i in range(1, DAYS+1):
+    for i in range(1, n_days+1):
         d = today - timedelta(days=i)
         d_str = d.isoformat()
         try:
@@ -37,5 +40,5 @@ def main():
             cnt_test = None
         data.append( (d_str, cnt, cnt_test) )
     
-    return render_template('main.html', data=data)
+    return render_template('main.html', n_days=n_days, data=data)
 
